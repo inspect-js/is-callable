b88d5439f1859c931f6cc6a0c34884a5e308a062
Jordan Harband <ljharb@gmail.com>
2015-01-29 09:37:37 -0800

    Add early exit for typeof not "function"

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var fnToStr = Function.prototype.toString;
 
 module.exports = function isCallable(value) {
+	if (typeof value !== 'function') { return false; }
 	try {
 		fnToStr.call(value);
 		return true;
