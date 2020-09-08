inspect-js/is-callable

commit 32e14cfd13f0cbb2ddd2cc613227b4e003745317

[New] use `Reflect.apply`‑based callability detection

.eslintrc
@@ -6,6 +6,7 @@
 	"rules": {
 		"id-length": 0,
 		"max-statements": [2, 12],
-		"max-statements-per-line": [2, { "max": 2 }]
-	}
+		"max-statements-per-line": [2, { "max": 2 }],
+		"operator-linebreak": [2, "before"],
+	},
 }

test/index.js
@@ -36,8 +36,11 @@ if (typeof Proxy === 'function') {
 		proxy();
 		String(proxy);
 	} catch (_) {
-		// Older engines throw a `TypeError` when `Function.prototype.toString` is called on a Proxy object.
-		proxy = null;
+		// If `Reflect` is supported, then `Function.prototype.toString` isn't used for callability detection.
+		if (typeof Reflect !== 'object') {
+			// Older engines throw a `TypeError` when `Function.prototype.toString` is called on a Proxy object.
+			proxy = null;
+		}
 	}
 }
 

index.js
@@ -1,6 +1,23 @@
 'use strict';
 
 var fnToStr = Function.prototype.toString;
+var reflectApply = typeof Reflect === 'object' && Reflect !== null && Reflect.apply;
+var badArrayLike;
+var isCallableMarker;
+if (typeof reflectApply === 'function' && typeof Object.defineProperty === 'function') {
+	try {
+		badArrayLike = Object.defineProperty({}, 'length', {
+			get: function () {
+				throw isCallableMarker;
+			}
+		});
+		isCallableMarker = {};
+	} catch (_) {
+		reflectApply = null;
+	}
+} else {
+	reflectApply = null;
+}
 
 var constructorRegex = /^\s*class\b/;
 var isES6ClassFn = function isES6ClassFunction(value) {
@@ -26,12 +43,24 @@ var fnClass = '[object Function]';
 var genClass = '[object GeneratorFunction]';
 var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
 
-module.exports = function isCallable(value) {
-	if (!value) { return false; }
-	if (typeof value !== 'function' && typeof value !== 'object') { return false; }
-	if (typeof value === 'function' && !value.prototype) { return true; }
-	if (hasToStringTag) { return tryFunctionObject(value); }
-	if (isES6ClassFn(value)) { return false; }
-	var strClass = toStr.call(value);
-	return strClass === fnClass || strClass === genClass;
-};
+module.exports = reflectApply
+	? function isCallable(value) {
+		if (!value) { return false; }
+		if (typeof value !== 'function' && typeof value !== 'object') { return false; }
+		if (typeof value === 'function' && !value.prototype) { return true; }
+		try {
+			reflectApply(value, null, badArrayLike);
+		} catch (e) {
+			if (e !== isCallableMarker) { return false; }
+		}
+		return !isES6ClassFn(value);
+	}
+	: function isCallable(value) {
+		if (!value) { return false; }
+		if (typeof value !== 'function' && typeof value !== 'object') { return false; }
+		if (typeof value === 'function' && !value.prototype) { return true; }
+		if (hasToStringTag) { return tryFunctionObject(value); }
+		if (isES6ClassFn(value)) { return false; }
+		var strClass = toStr.call(value);
+		return strClass === fnClass || strClass === genClass;
+	};

commit 579179efde4cc5ee0c8288a416b4848380b7f174

[Fix] phantomjs‘ Reflect.apply does not throw properly on a bad array-like
Caused by #54; see https://github.com/enzymejs/enzyme/issues/2439

.eslintrc
@@ -9,4 +9,13 @@
 		"max-statements-per-line": [2, { "max": 2 }],
 		"operator-linebreak": [2, "before"],
 	},
+
+	"overrides": [
+		{
+			"files": "test/**",
+			"rules": {
+				"no-throw-literal": 0,
+			},
+		},
+	],
 }

test/index.js
@@ -181,3 +181,10 @@ test('proxies of functions', { skip: !proxy }, function (t) {
 	t.ok(isCallable(proxy), 'proxies of functions are callable');
 	t.end();
 });
+
+test('throwing functions', function (t) {
+	t.plan(1);
+
+	var thrower = function (a) { return a.b; };
+	t.ok(isCallable(thrower), 'a function that throws is callable');
+});